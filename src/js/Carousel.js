d8fe280828ca96f88e88bd92433f4ce7147f1091
Cheryl Fernandes <cherylfernandes@Cheryls-MacBook-Air.local>
2018-08-01 17:18:26 -0700

    Start adding functionality to the gallery

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -1,21 +1,53 @@
 import React, { PropTypes, Component } from 'react';
+import CarouselContainer from './CarouselContainer'
+import Wrapper from './Wrapper'
+import CarouselSlot from './CarouselSlot'
 import Item from './Item'
 
 class Carousel extends Component {
 
 constructor(props){
   super(props)
+  this.state = {
+    position: 0,
+    direction: props.children.length === 2 ? 'prev' : 'next',
+    sliding: false,
+    firstTime: true
+  }  
+}
+
+
+getOrder(itemIndex) {
+  const { position } = this.state
+  const { children } = this.props
+  const numItems = children.length || 1
+  if (itemIndex - position < 0) {
+    return numItems - Math.abs(itemIndex - position)
+  }
+  return itemIndex - position
 }
 
   render() {
-    const { title, children } = this.props
+const { title, children } = this.props
+    const { sliding, direction, position, firstTime} = this.state  
   
     return (
       <div>
         <h2 style={{textAlign: 'center'}}>{ title }</h2>
+        <Wrapper>
+            <CarouselContainer
+                sliding={ sliding }
+                direction={ direction }
+                firstTime={ firstTime }>
               { children.map((child, index) => (
-                  <Item url={child.url} caption={child.caption} key={index}/>
-              )) }         
+                <CarouselSlot
+                  key={ index }
+                  order={ this.getOrder(index) }>
+                  <Item url={child.url} caption={child.caption} order={ this.getOrder(index) }/>
+                </CarouselSlot>
+              )) }
+            </CarouselContainer>      
+        </Wrapper>      
         </div>
     )
   }
