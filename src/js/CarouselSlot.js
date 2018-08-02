e15226816a10a7417ee3b5972944b6163e438270
Cheryl Fernandes <cherylfernandes@Cheryls-MacBook-Air.local>
2018-08-01 17:27:23 -0700

    Styled image so that it resizes with the screen size

--- src/js/CarouselSlot.js
+++ src/js/CarouselSlot.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 const CarouselSlot = styled.div`
   flex: 1 0 100%;
-  flex-basis: 80%;
   margin-right: 20px;
   text-align: center;
   order: ${(props) =>  props.order};
